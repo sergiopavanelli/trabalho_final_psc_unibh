81a0a1495b8c67933d87dd72a202018a2e574e4e
Sergio Pavanelli <sergiopavanelli@MacBook-Pro-de-Sergio.local>
2024-05-28 14:52:09 -0300

    class Doacao

--- Sistema_doacoes/README.md
+++ Sistema_doacoes/README.md
@@ -2,3 +2,5 @@ Alunos:
 
 Sérgio Pinton Pavanelli - RA 123220202;
 Klaus Boger Tolentino Juliani - RA 122211902
+
+
---
+++ Sistema_doacoes/src/Doacao.java
@@ -0,0 +1,44 @@
+import java.util.Date;
+
+public class Doacao {
+    private String tipo;
+    private double quantidade;
+    private Date data;
+
+    // Construtor
+    public Doacao(String tipo, double quantidade, Date data) {
+        this.tipo = tipo;
+        this.quantidade = quantidade;
+        this.data = data;
+    }
+
+    // Getters e Setters
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Doacao [tipo=" + tipo + ", quantidade=" + quantidade + ", data=" + data + "]";
+    }
+}
